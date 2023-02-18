ecf1ff947baa79a8654cafc3352b3240132dc7b9
Matt Gleich <git@mattglei.ch>
2023-02-17 19:20:56 -0500

    feat: basic buttons
    Signed-off-by: Matt Gleich <git@mattglei.ch>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div class="root">
+	<main>
+		<h1>Quick Math</h1>
+
+		<div class="buttons">
+			<button> Addition </button>
+			<button>Subtraction</button>
+			<button>Multiplication</button>
+			<button>Division</button>
+			<button>Everything</button>
+		</div>
+	</main>
+</div>
+
+<style>
+	.root {
+		display: flex;
+		width: 100vw;
+		height: 100vh;
+		overflow-x: hidden;
+		align-items: center;
+		justify-content: center;
+	}
+
+	button {
+		background-color: var(--border);
+		border: 0.5px solid rgb(67, 67, 67);
+		color: var(--foreground);
+	}
+</style>
